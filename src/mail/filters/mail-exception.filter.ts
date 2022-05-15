1c4588abdd2c90de97240adaf19058f1c810f2d0
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-05-15 22:31:04 +0700

    edit response

--- src/mail/filters/mail-exception.filter.ts
+++ src/mail/filters/mail-exception.filter.ts
@@ -1,10 +1,17 @@
 import { Catch, RpcExceptionFilter, ArgumentsHost } from '@nestjs/common';
-import { Observable, throwError } from 'rxjs';
+import { Observable, throwError, of } from 'rxjs';
 import { RpcException } from '@nestjs/microservices';
+import { IRes } from '../interfaces/IRes';
 
 @Catch(RpcException)
 export class MailExceptionFilter implements RpcExceptionFilter<RpcException> {
     catch(exception: RpcException, host: ArgumentsHost): Observable<any> {
-        return throwError(() => exception.getError());
+        const message = exception.getError() as string;
+        const res: IRes = {
+            message,
+            status: 'error'
+        };
+        const resStr = JSON.stringify(res);
+        return of(resStr);
     }
 }
