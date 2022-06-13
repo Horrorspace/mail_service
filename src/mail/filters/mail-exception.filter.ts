2d13a097c15fe0072be01d8eeff6c67627b61778
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-06-13 17:53:41 +0700

    refactoring

--- src/mail/filters/mail-exception.filter.ts
+++ src/mail/filters/mail-exception.filter.ts
@@ -1,15 +1,16 @@
 import { Catch, RpcExceptionFilter } from '@nestjs/common';
 import { Observable, of } from 'rxjs';
 import { RpcException } from '@nestjs/microservices';
-import { IRes } from '../interfaces/IRes';
+import { IErrorRes, codeRes } from '../interfaces/IErrorRes';
+import { statuses } from '../enums/statuses.enum';
 
 @Catch(RpcException)
 export class MailExceptionFilter implements RpcExceptionFilter<RpcException> {
     public catch(exception: RpcException): Observable<any> {
-        const message = exception.getError() as string;
-        const res: IRes = {
+        const message = exception.getError() as codeRes;
+        const res: IErrorRes = {
             message,
-            status: 'error',
+            status: statuses.error,
         };
         const resStr = JSON.stringify(res);
         return of(resStr);
--- src/mail/pipes/json.pipe.ts
+++ src/mail/pipes/json.pipe.ts
@@ -1,6 +1,7 @@
 import { PipeTransform, Injectable } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
 import { UserConfirmDto } from '../dto/user-confirm.dto';
+import { codes } from '../enums/codes.enum';
 
 @Injectable()
 export class JsonPipe implements PipeTransform {
@@ -8,18 +9,18 @@ export class JsonPipe implements PipeTransform {
         if (typeof value === 'string') {
             const data = JSON.parse(value) as UserConfirmDto;
             if (!data.hasOwnProperty('email')) {
-                throw new RpcException('400');
+                throw new RpcException(codes.badRequest);
             }
             if (!data.hasOwnProperty('code')) {
-                throw new RpcException('400');
+                throw new RpcException(codes.badRequest);
             }
             if (typeof data.email !== 'string') {
-                throw new RpcException('400');
+                throw new RpcException(codes.badRequest);
             }
             if (typeof data.code !== 'string') {
-                throw new RpcException('400');
+                throw new RpcException(codes.badRequest);
             }
             return data;
-        } else throw new RpcException('400');
+        } else throw new RpcException(codes.badRequest);
     }
 }
