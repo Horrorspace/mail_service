da960e1f6b1af9cc1a9ad1168e1dcb9932b9731f
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-06-13 15:51:53 +0700

    remove test code

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -12,14 +12,7 @@ export class MailService {
     constructor(
         @Inject(MailerService) private readonly mailerService: MailerService,
         @Inject(services.logger) private readonly clinet: ClientProxy,
-    ) {
-        setInterval(() => {
-            this.clinet.send('info', 'test').subscribe({
-                next: (val) => console.log(val)
-            });
-            console.log('send');
-        }, 10000)
-    }
+    ) {}
 
     public async sendConfirmCode({
         email,
