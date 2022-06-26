8a581dbc5144714559b2fb1016e2cf5f3682592e
Urkin Eugeniy <utkineugeniy@gmail.com>
2022-06-26 20:54:36 +0700

    edit output type

--- src/mail/pipes/json.pipe.ts
+++ src/mail/pipes/json.pipe.ts
@@ -1,14 +1,13 @@
 import { PipeTransform, Injectable } from '@nestjs/common';
 import { RpcException } from '@nestjs/microservices';
-import { UserConfirmDto } from '../dto/user-confirm.dto';
 import { codes } from '../enums/codes.enum';
 import { IError } from '../interfaces/IError';
 
 @Injectable()
 export class JsonPipe implements PipeTransform {
-    public transform(value: any): UserConfirmDto {
+    public transform(value: any): object {
         if (typeof value === 'string') {
-            const data = JSON.parse(value) as UserConfirmDto;
+            const data = JSON.parse(value) as object;
             return data;
         } else {
             const error: IError = {
